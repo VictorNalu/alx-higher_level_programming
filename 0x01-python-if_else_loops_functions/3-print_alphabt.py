2c2aad3b1ebf117acc3263f0b02db8d6dedd29f9
VictorNalu <victorchinalurumogu@gmail.com>
2023-11-28 14:48:32 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for b in range(97, 123):
-    if chr(b) not in ('q' , 'e'):
-        print("{}" .format (chr(b)), end='')
+    if b in ('q', 'e'):
+        continue
+    print("{}" .format (chr(b)), end='')
