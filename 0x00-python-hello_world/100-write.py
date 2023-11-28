6f3299cf03072cfa01ea44e770e5cf3f2aec93f7
VictorNalu <victorchinalurumogu@gmail.com>
2023-11-27 16:56:03 -0800

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 feed = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-#writes to stderr using sys.stderr.write
+# Writes to stderr using sys.stderr.write
 sys.stderr.write(feed)
 # Exits with status code 1
 sys.exit(1)
