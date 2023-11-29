44d209c1fd84fc30ddbc0207eb84708bcf9c2cc9
VictorNalu <victorchinalurumogu@gmail.com>
2023-11-28 20:52:33 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for first_num in range(10):
+for first_num in range(0, 10):
     for second_num in range(first_num + 1, 10):
         if first_num == 8 and second_num == 9:
             print("{}{}".format(first_num, second_num))
-    else:
+        else:
             print("{}{}, ".format(first_num, second_num, end=''))
