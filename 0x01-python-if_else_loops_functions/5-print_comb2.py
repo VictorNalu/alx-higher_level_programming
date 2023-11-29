c7ad8d0d0fa6bd514f0798660011dbfac219f656
VictorNalu <victorchinalurumogu@gmail.com>
2023-11-28 18:49:38 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for b in range(100):
     if b == 99:
-        print("{}" .format (b))
+        print("{}" .format(b))
     else:
-        print("{:02d}, " .format (b), end='')
+        print("{:02d}, " .format(b), end='')
