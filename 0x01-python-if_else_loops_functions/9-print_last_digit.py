a5d7910106e3e222eda9591c90864e1866d6f99c
VictorNalu <victorchinalurumogu@gmail.com>
2023-11-29 00:34:03 -0800

    9-print_last_digit.py && 10-add.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-       last_dit = (-number % 10)
+        last_dit = (-number % 10)
     elif number >= 0:
-         last_dit = number % 10
+        last_dit = number % 10
     print("{}".format(last_dit), end="")
     return last_dit
