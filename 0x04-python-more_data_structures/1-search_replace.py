f0c0e16471b6ce676f063cfa24be5419d8db01d9
VictorNalu <victorchinalurumogu@gmail.com>
2023-12-06 23:32:37 +0100

    0x04 python (a)

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     if my_list == [] or my_list is None:
         return []
-    new_list = [replace if element is search else element for element in my_list]
+    new_list = [replace if elmnt is search else elmnt for elmnt in my_list]
     return new_list
