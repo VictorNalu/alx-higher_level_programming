73be6c4e2a8688637bc592cf4dc265d8802f272f
VictorNalu <victorchinalurumogu@gmail.com>
2023-12-22 05:00:59 +0100

    obj_class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,53 +1,48 @@
-#!/usr/bin/python3
-"""Define a MagicClass that does exactly as the bytecode provided."""
-
 import math
 
-
 class MagicClass:
-    """Represent a circle."""
+    """
+    MagicClass: A class that represents a magical shape.
 
-    def __init__(self, radius=0):
-        """Initialize a MagicClass.
+    Attributes:
+        __magic_radius (int or float): The magical radius of the shape.
 
-        Arg:
-            radius (float or int): The radius of the new MagicClass.
+    Methods:
+        __init__(self, magic_radius=0): Initializes the MagicClass instance with a given magical radius.
+        calculate_area(self): Calculates and returns the magical area of the shape.
+        calculate_circumference(self): Calculates and returns the magical circumference of the shape.
+    """
+
+    def __init__(self, magic_radius=0):
         """
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self.__radius = radius
+        Initializes a new MagicClass instance.
 
-    def area(self):
-        """Return the area of the MagicClass."""
-        return (self.__radius ** 2 * math.pi)
+        Args:
+            magic_radius (int or float): The magical radius of the shape.
+                                         Defaults to 0.
 
-    def circumference(self):
-        """Return The circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius)#!/usr/bin/python3
-"""Define a MagicClass that does exactly as the bytecode provided."""
-
-import math
-
-
-class MagicClass:
-    """Represent a circle."""
-
-    def __init__(self, radius=0):
-        """Initialize a MagicClass.
-
-        Arg:
-            radius (float or int): The radius of the new MagicClass.
+        Raises:
+            TypeError: If the provided magical radius is not a number.
         """
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self.__radius = radius
+        if type(magic_radius) is not int and type(magic_radius) is not float:
+            raise TypeError('magic_radius must be a number')
 
-    def area(self):
-        """Return the area of the MagicClass."""
-        return (self.__radius ** 2 * math.pi)
+        self.__magic_radius = magic_radius
 
-    def circumference(self):
-        """Return The circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius)
+    def calculate_area(self):
+        """
+        Calculates and returns the magical area of the shape.
+
+        Returns:
+            float: The magical area of the shape.
+        """
+        return self.__magic_radius ** 2 * math.pi
+
+    def calculate_circumference(self):
+        """
+        Calculates and returns the magical circumference of the shape.
+
+        Returns:
+            float: The magical circumference of the shape.
+        """
+        return 2 * math.pi * self.__magic_radius
