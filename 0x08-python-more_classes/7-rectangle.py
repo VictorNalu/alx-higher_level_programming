b7e670de86c29e35cdac5b9f10a9402d643965ba
VictorNalu <victorchinalurumogu@gmail.com>
2024-02-13 04:33:40 +0100

    change_representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """class rectangle"""
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -66,4 +66,3 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
