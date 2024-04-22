3a837cb5d81b45d650922c02ac3ee0d4a5b47820
VictorNalu <victorchinalurumogu@gmail.com>
2024-04-22 16:56:59 +0100

    JS_warmup(6)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const num = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const num = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of num) {
   console.log(i);
 }
