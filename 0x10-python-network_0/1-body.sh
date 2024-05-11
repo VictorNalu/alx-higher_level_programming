972d16ad3a6877c5149693f1df8a99c89cbc2431
VictorNalu <victorchinalurumogu@gmail.com>
2024-05-11 06:09:03 +0100

    bod_net1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o response.txt -w "%{http_code}" "$1" >/dev/null
+curl -sL "$1"
