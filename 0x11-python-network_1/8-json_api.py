f544869d4a23864b9640933957c1d367f80d65a9
VictorNalu <victorchinalurumogu@gmail.com>
2024-05-12 05:17:48 +0100

    network_

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-if __name__ == "__main":
+if __name__ == "__main__":
 
     # Set default value for q if no argument is given
     q = sys.argv[1] if len(sys.argv) > 1 else ""
