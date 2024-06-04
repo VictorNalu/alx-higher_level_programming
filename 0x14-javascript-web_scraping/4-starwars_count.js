f61325e4664da6fef9b37c3b98363a3eee6728b8
VictorNalu <victorchinalurumogu@gmail.com>
2024-06-04 19:45:43 +0100

    js_0004

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
